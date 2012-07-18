fd336c6b91205f7506bb32a4d8122a51301dd0ec
Ross Light <ross@zombiezen.com>
2012-07-18 07:44:43 -0700

    Use standard math package style of assembly

---
+++ vec64/op_386.s
@@ -0,0 +1,23 @@
+// vec64/op_386.s
+// vim: ft=9asm ai
+
+// func Add(v1, v2 Vector) Vector
+// v1: +0(FP)
+// v2: +24(FP)
+// Return: +48(FP)
+TEXT ·Add(SB),$0-72
+    JMP         ·add(SB)
+
+// func Sub(v1, v2 Vector) Vector
+// v1: +0(FP)
+// v2: +24(FP)
+// Return: +48(FP)
+TEXT ·Sub(SB),$0-72
+    JMP         ·sub(SB)
+
+// func Dot(v1, v2 Vector) float64
+// v1: +0(FP)
+// v2: +24(FP)
+// Return: +48(FP)
+TEXT ·Dot(SB),$0-56
+    JMP         ·dot(SB)
---
+++ vec64/op_arm.s
@@ -0,0 +1,23 @@
+// vec64/op_arm.s
+// vim: ft=9asm ai
+
+// func Add(v1, v2 Vector) Vector
+// v1: +0(FP)
+// v2: +24(FP)
+// Return: +48(FP)
+TEXT ·Add(SB),$0-72
+    B           ·add(SB)
+
+// func Sub(v1, v2 Vector) Vector
+// v1: +0(FP)
+// v2: +24(FP)
+// Return: +48(FP)
+TEXT ·Sub(SB),$0-72
+    B           ·sub(SB)
+
+// func Dot(v1, v2 Vector) float64
+// v1: +0(FP)
+// v2: +24(FP)
+// Return: +48(FP)
+TEXT ·Dot(SB),$0-56
+    B           ·dot(SB)
