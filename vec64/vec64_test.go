39d8027c93900b2b07fc0a2d341f926deb3442af
Ross Light <ross@zombiezen.com>
2012-07-18 08:53:10 -0700

    Clean up unit tests (more table-driven)

--- vec64/vec64_test.go
+++ vec64/vec64_test.go
@@ -64,116 +64,96 @@ func BenchmarkDot(b *testing.B) {
 }
 
 func TestNormalize(t *testing.T) {
-	comps := []float64{1.0, 2.0, -2.0}
-	length := 3.0
-	v := Vector{comps[0], comps[1], comps[2]}
-	vn := v.Normalize()
-	if vn.Length() != 1.0 {
-		t.Error("Length is not 1")
+	tests := []struct {
+		V        Vector
+		Expected Vector
+	}{
+		{Vector{1.0, 2.0, -2.0}, Vector{1.0 / 3.0, 2.0 / 3.0, -2.0 / 3.0}},
 	}
-	for axis := 0; axis <= 2; axis++ {
-		if vn[axis] != comps[axis]/length {
-			t.Error("0 component is incorrect")
+	for _, tt := range tests {
+		if result := tt.V.Normalize(); result != tt.Expected {
+			t.Errorf("%v.Normalize() != %v (got %v)", tt.V, tt.Expected, result)
 		}
 	}
 }
 
 func TestLength(t *testing.T) {
-	type lengthTest struct {
+	tests := []struct {
 		Vec       Vector
 		Length    float64
 		LengthSqr float64
+	}{
+		{Vector{0.0, 0.0, 0.0}, 0.0, 0.0},
+
+		{Vector{1.0, 0.0, 0.0}, 1.0, 1.0},
+		{Vector{0.0, 1.0, 0.0}, 1.0, 1.0},
+		{Vector{0.0, 0.0, 1.0}, 1.0, 1.0},
+
+		{Vector{-1.0, 0.0, 0.0}, 1.0, 1.0},
+		{Vector{0.0, -1.0, 0.0}, 1.0, 1.0},
+		{Vector{0.0, 0.0, -1.0}, 1.0, 1.0},
+
+		{Vector{3.0, -4.0, 0.0}, 5.0, 25.0},
+		{Vector{1.0, 2.0, -2.0}, 3.0, 9.0},
+		{Vector{3.14, 20.7, 0.5}, 20.942769635365803, 438.59959999999995},
 	}
 
-	tests := []lengthTest{
-		lengthTest{Vector{0.0, 0.0, 0.0}, 0.0, 0.0},
-
-		lengthTest{Vector{1.0, 0.0, 0.0}, 1.0, 1.0},
-		lengthTest{Vector{0.0, 1.0, 0.0}, 1.0, 1.0},
-		lengthTest{Vector{0.0, 0.0, 1.0}, 1.0, 1.0},
-
-		lengthTest{Vector{-1.0, 0.0, 0.0}, 1.0, 1.0},
-		lengthTest{Vector{0.0, -1.0, 0.0}, 1.0, 1.0},
-		lengthTest{Vector{0.0, 0.0, -1.0}, 1.0, 1.0},
-
-		lengthTest{Vector{3.0, -4.0, 0.0}, 5.0, 25.0},
-		lengthTest{Vector{1.0, 2.0, -2.0}, 3.0, 9.0},
-		lengthTest{Vector{3.14, 20.7, 0.5}, 20.942769635365803, 438.59959999999995},
-	}
-
-	for _, ltest := range tests {
-		if ltest.Vec.LengthSqr() != ltest.LengthSqr {
-			t.Errorf("LengthSqr failed for %v (wanted %.2f, got %.2f)", ltest.Vec, ltest.LengthSqr, ltest.Vec.LengthSqr())
+	for _, tt := range tests {
+		if l2 := tt.Vec.LengthSqr(); l2 != tt.LengthSqr {
+			t.Errorf("%v.LengthSqr() != %v (got %#v)", tt.Vec, tt.LengthSqr, l2)
 		}
-		if ltest.Vec.Length() != ltest.Length {
-			t.Errorf("Length failed for %v (wanted %.2f, got %.2f)", ltest.Vec, ltest.Length, ltest.Vec.Length())
+		if l := tt.Vec.Length(); l != tt.Length {
+			t.Errorf("%v.Length() != %v (got %#v)", tt.Vec, tt.Length, l)
 		}
 	}
 }
 
 func TestAbs(t *testing.T) {
-	var v Vector
-
-	v = Vector{0, 0, 0}.Abs()
-	if v[0] != 0 || v[1] != 0 || v[2] != 0 {
-		t.Error("Zero vector incorrect")
+	tests := []struct {
+		Vec      Vector
+		Expected Vector
+	}{
+		{Vector{0, 0, 0}, Vector{0, 0, 0}},
+		{Vector{1, 2, 3}, Vector{1, 2, 3}},
+		{Vector{-1, -2, -3}, Vector{1, 2, 3}},
+		{Vector{-1, 2, -3}, Vector{1, 2, 3}},
 	}
-
-	v = Vector{1, 2, 3}.Abs()
-	if v[0] != 1 || v[1] != 2 || v[2] != 3 {
-		t.Error("All positive vector incorrect")
-	}
-
-	v = Vector{-1, -2, -3}.Abs()
-	if v[0] != 1 || v[1] != 2 || v[2] != 3 {
-		t.Error("All negative vector incorrect")
-	}
-
-	v = Vector{-1, 2, -3}.Abs()
-	if v[0] != 1 || v[1] != 2 || v[2] != 3 {
-		t.Error("Mixed vector incorrect")
+	for _, tt := range tests {
+		if result := tt.Vec.Abs(); result != tt.Expected {
+			t.Errorf("%v.Abs() != %v (got %v)", tt.Vec, tt.Expected, result)
+		}
 	}
 }
 
 func TestNegate(t *testing.T) {
-	var v Vector
-
-	v = Vector{0, 0, 0}.Negate()
-	if v[0] != 0 || v[1] != 0 || v[2] != 0 {
-		t.Error("Zero vector incorrect")
+	tests := []struct {
+		Vec      Vector
+		Expected Vector
+	}{
+		{Vector{0, 0, 0}, Vector{0, 0, 0}},
+		{Vector{1, 2, 3}, Vector{-1, -2, -3}},
+		{Vector{-1, -2, -3}, Vector{1, 2, 3}},
+		{Vector{-1, 2, -3}, Vector{1, -2, 3}},
 	}
-
-	v = Vector{1, 2, 3}.Negate()
-	if v[0] != -1 || v[1] != -2 || v[2] != -3 {
-		t.Error("All positive vector incorrect")
-	}
-
-	v = Vector{-1, -2, -3}.Negate()
-	if v[0] != 1 || v[1] != 2 || v[2] != 3 {
-		t.Error("All negative vector incorrect")
-	}
-
-	v = Vector{-1, 2, -3}.Negate()
-	if v[0] != 1 || v[1] != -2 || v[2] != 3 {
-		t.Error("Mixed vector incorrect")
+	for _, tt := range tests {
+		if result := tt.Vec.Negate(); result != tt.Expected {
+			t.Errorf("%v.Negate() != %v (got %v)", tt.Vec, tt.Expected, result)
+		}
 	}
 }
 
 func TestIsZero(t *testing.T) {
-	var v Vector
-
-	v = Vector{0, 0, 0}
-	if !v.IsZero() {
-		t.Error("Zero vector is not zero")
+	tests := []struct {
+		Vec      Vector
+		Expected bool
+	}{
+		{Vector{0, 0, 0}, true},
+		{Vector{1, 0, 0}, false},
+		{Vector{-1, 0, 0}, false},
 	}
-
-	v = Vector{1, 0, 0}
-	if v.IsZero() {
-		t.Error("Positive 0 vector is zero")
-	}
-
-	v = Vector{-1, 0, 0}
-	if v.IsZero() {
-		t.Error("Negative 0 vector is zero")
+	for _, tt := range tests {
+		if tt.Vec.IsZero() != tt.Expected {
+			t.Errorf("%v.IsZero() != %v", tt.Vec, tt.Expected)
+		}
 	}
 }
